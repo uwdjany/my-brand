a7f1ce165bf0a43bcc08d314ecc117b498e06c84
uwdjany <djanatiuwase@gmail.com>
2023-02-01 23:03:53 +0200

    contact page added

--- js/index.js
+++ js/index.js
@@ -28,6 +28,77 @@ function activeMenu(){
 activeMenu()
 window.addEventListener("scroll",activeMenu);
 
+const slideRow = document.querySelector('.content-row');
+const slide = document.querySelector('.col-slider');
+const nextBtn = document.querySelector('#next-btn');
+const prevBtn = document.querySelector('#prev-btn');
+const interval = 3000;
+let sliderId;
+
+let slides = document.querySelectorAll('.slide')
+let index = 1;
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
+firstClone.id ='first-clone';
+lastClone.id ='last-clone';
+
+slide.append(firstClone);
+slide.append(lastClone);
+
+const slideWidth = slides[index].clientWidth;
+slide.style.transform = `translateX(${-slideWidth * index}px)`;
+
+const startSlide =()=>{
+  sliderId = setInterval(()=>{
+        moveToNextSlide();
+    },interval)
+};
+
+const getSlides =()=>  document.querySelectorAll('.slide')
+
+
+
+slide.addEventListener('transitionend',()=>{
+    slides = getSlides();
+    if(slides[index].id === firstClone.id){
+        slide.style.transition ='none';
+        index = 1;
+        slide.style.transform = `translateX(${-slideWidth * index}px)`;
+        
+         
+    }
+
+    if(slides[index].id === lastClone.id){
+        slide.style.transition ='none';
+        index = slides.length - 2;
+        slide.style.transform = `translateX(${-slideWidth * index}px)`;
+        
+         
+    }
+
+})
+const moveToNextSlide=()=>{
+    slides = getSlides();
+    if(index >= slides.length -1) return;
+    index++;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+slide.style.transition='.7s';
+
+};
+const moveToPreviosSlide=()=>{
+    // if(index <= 0) return;
+    index--;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+slide.style.transition='.7s';
+
+}
+slideRow.addEventListener('mouseenter', ()=>{
+    clearInterval(sliderId);
+})
+slideRow.addEventListener('mouseleave',startSlide)
+nextBtn.addEventListener('click',moveToNextSlide);
+prevBtn.addEventListener('click', moveToPreviosSlide);
+ startSlide();
 
 
 
