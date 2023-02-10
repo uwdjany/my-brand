653a52753be860e99c34fe5864b2f079b963570d
uwdjany <djanatiuwase@gmail.com>
2023-02-10 11:35:08 +0200

    Retrieve user from localStorage

--- js/authentication.js
+++ js/authentication.js
@@ -3,6 +3,9 @@ const signUp = (e) => {
   let lname = document.getElementById("lname").value;
   let email = document.getElementById("email").value;
   let pwd = document.getElementById("pwd").value;
+ 
+  const role = "visitor";
+
 
   let formData = JSON.parse(localStorage.getItem("formData")) || [];
   let exist =
@@ -13,7 +16,7 @@ const signUp = (e) => {
         data.lname.toLowerCase() == lname.toLowerCase()
     );
   if (!exist) {
-    formData.push({ fname, lname, email, pwd });
+    formData.push({ fname, lname, email, role, pwd });
     localStorage.setItem("formData", JSON.stringify(formData));
     document.querySelector("form").reset();
     document.getElementById("fname").focus();
@@ -21,30 +24,61 @@ const signUp = (e) => {
   } else {
     alert("Ooopppssss..\nYou have already signed up");
   }
-  displayData()
   e.preventDefault();
 };
+let tbody = document.querySelector('tbody');
+function displayData() {
+  tbody.innerHTML = '';
+  const users = JSON.parse(localStorage.getItem("formData"));
+  users.forEach((user,i) => {
+    //console.log(user);
+   tbody.innerHTML +=`<tr>
+   <td>${++i}</td>
+   <td>${user.fname}</td>
+   <td>${user.lname}</td>
+   <td>${user.email}</td>
+   <td>${user.pwd}</td>
+   <td>${user.role}</td>
+   <td>
+   <button  style="background:red ; color:white; padding:6px 9px; border-radius:8px;" type="button"  onclick="deleteUser(${--i});">Delete</button>
+   </td>
+   </tr>
+   `
+  });
+}
 
-
-
-
-function displayData(){
-  console.log(JSON.parse(localStorage.getItem('formData')))
+deleteUser = i =>{
+  if(confirm('Are you sure You want to delete this User')){
+    data.splice(++i, 1);
+    localStorage.setItem('formData',JSON.stringify(data))
+    displayData()
+  }
 }
 
 
 
-
 function signIn(e) {
-    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
-    let formData = JSON.parse(localStorage.getItem('formData')) || [];
-    let exist = formData.length && 
-    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
-    if(!exist){
-        alert("Invalid email or password");
+  let email = document.getElementById("email").value,
+    pwd = document.getElementById("pwd").value;
+  let formData = JSON.parse(localStorage.getItem("formData")) || [];
+  let exist =
+    formData.length &&
+    JSON.parse(localStorage.getItem("formData")).some(
+      (data) =>
+        data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd
+    );
+  console.log(exist);
+  if (!exist) {
+    alert("Invalid email or password");
+  } else {
+    const user = JSON.parse(localStorage.getItem("formData")).filter(
+      (item) => item.email == email && item.pwd == pwd
+    )[0];
+    if (user.role.toLowerCase() == "admin") {
+      location.href = "/dashboard.html";
+    } else {
+      location.href = "/blog.html";
     }
-    else{
-        location.href = "/blog.html";
-    }
-    e.preventDefault();
+  }
+  e.preventDefault();
 }
